c26dc81772ccb56dcc3467b5f82a6ba350e8f87d
Ella <emmanuellaumoye18@gmail.com>
2023-08-29 01:57:03 -0400

    Creating new files for portfolio site

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+/**
+ * TODO: Include documentation for what this file does
+ * @returns 
+ */
+const App =() => {
+
+  return (
+      <div>
+        Emmanuella Developer Site
+      </div>
+  )
+}
+
+export default App
